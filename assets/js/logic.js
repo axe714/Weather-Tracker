1ae9bc94ef17237e96437f04e5a373e8ba94efc4
Axe714 <axearzadon@gmail.com>
2022-04-29 12:35:24 -0700

    Added functions to fetch and display current weather data for chosen city

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,7 @@
-var oneCallBaseEndPoint = 'https://api.openweathermap.org/data/2.5/onecall'
+var oneCallBaseEndPoint = 'https://api.openweathermap.org/data/2.5/onecall';
 var weatherBaseEndPoint = 'https://api.openweathermap.org/data/2.5/weather';
 var API_KEY = '180c9f853ac8fcc595fe4080e0abf997';
+var units = 'imperial'
 
 function getWeather(city) {
     fetch(weatherBaseEndPoint + `?q=${encodeURI(city)}&appid=${API_KEY}`)
@@ -10,13 +11,27 @@ function getWeather(city) {
             var lon = weatherData.coord.lon;
             console.log('weather data', weatherData)
             console.log('lat and lon', { lat, lon })
-            fetch(oneCallBaseEndPoint + `?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+            fetch(oneCallBaseEndPoint + `?lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`)
                 .then(oneCallRes => oneCallRes.json())
                 .then(oneCallData => {
-                    var currentWeather = oneCallData.current.weather[0].main
-                    console.log('currentWeather', currentWeather)
-                    document.querySelector('#today-cloud-condition').textContent += ": " + currentWeather
+                    
+                    var currentCloud = oneCallData.current.weather[0].main
+                    console.log('currentCloud', currentCloud)
+                    document.querySelector('#today-cloud-condition').textContent += ": " + currentCloud
                     console.log(oneCallData)
+
+                    var currentTemperature = oneCallData.current.temp
+                    console.log(currentTemperature)
+                    document.querySelector('#today-temp').textContent += ": " + currentTemperature + "℉"
+
+                    var currentHumidity = oneCallData.current.humidity
+                    console.log(currentHumidity)
+                    document.querySelector('#today-humidity').textContent += ": " + currentHumidity
+                    
+                    var currentWindSpeed = oneCallData.current.wind_speed
+                    console.log(currentWindSpeed)
+                    document.querySelector('#today-wind').textContent += ": " + currentWindSpeed
+
                 })
         })
 }
